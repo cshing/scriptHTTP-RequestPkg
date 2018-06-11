f781cc7b84f693f800545733e11a3514f38c8f14
Carmen Shing <carmenshing@gmail.com>
2018-06-11 22:01:26 +0000

    added http request function, pipe image file

--- HTTP-request.js
+++ HTTP-request.js
@@ -1,7 +1,7 @@
 var request = require('request');
 var fs = require('fs');
 
-request.get('https://sytantris.github.io/http-examples')
+request.get('https://sytantris.github.io/http-examples/future.jpg')
        .on('error', function (err) {
        })
 
@@ -11,10 +11,12 @@ request.get('https://sytantris.github.io/http-examples')
 
        .on('response', function (response) {
         console.log(response.statusMessage);
+        console.log('Downloading image...');
       })
 
       .on('response', function (response) {
-        console.log(response.statusheaders['content-type']);
+        console.log(response.headers['content-type'])
+        console.log('Download complete.');
       })
 
-       .pipe(fs.createWriteStream('./future.jpg'));
+      .pipe(fs.createWriteStream('./future.jpg'));
